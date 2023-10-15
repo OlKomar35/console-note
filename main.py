712dada3dbe120c1824217f88743c28cd736e13e
Olga Komar <komar_ol@mail.ru>
2023-10-15 13:16:54 +0300

    commit for pull request

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 import os
 from datetime import datetime
 
+
 def help_print_cmd():
     print()
     print('Добавление новой заметки')
@@ -12,6 +13,12 @@ def help_print_cmd():
     print()
     print('Показать список всех заметок')
     print('list')
+    print()
+    print('Показать список заметок, созданных/отредактированных в определенную дату')
+    print('list --date <Дата>')
+    print()
+    print('Прочитать заметку из файла')
+    print('read --id <Номер>')
     print()
     print('Удалить заметку по ее номеру')
     print('remove --id <Номер>')
@@ -27,9 +34,28 @@ def help_print_cmd():
     print('exit')
 
 
-flag = True
-id_ = 0
 list_data = list()
+id_ = 0
+current_dir = os.getcwd()  # текущая папка
+for filename in os.listdir(current_dir):
+    if filename.startswith('note_') and filename.endswith('.json'):
+        file_path = os.path.join(current_dir, filename)
+        if os.path.isfile(file_path):  # полный путь к файлу
+
+            # Чтение содержимого JSON-файла в строку
+            with open(filename) as json_file:
+                json_str = json_file.read()
+
+            # Распарсить JSON-строку в объект Python
+            data = json.loads(json_str)
+
+            # Нахождение Id
+            result = filename.split("_")
+            id_ = int(result[-1].split(".")[0])
+            list_data.append(data)
+
+flag = True
+list_id = list()
 data = {}
 json_string = ''
 
@@ -68,15 +94,30 @@ while flag:
             print('Нет данных для сохранения')
 
     elif list_cmd[0].replace(' ', '').lower() == 'list':
-        if list_data:
+        size_ = len(list_cmd)
+
+        if size_ == 1:
+            if list_data:
+                sorted_list = sorted(list_data, key=lambda x: x['date'])
+                for item in sorted_list:
+                    print(f"id:{item['id']} - {item['title']} - {item['msg']} - {item['date']} ")
+                    print()
+            else:
+                print('Заметок нет')
+        elif size_ == 2:
+            date_selected = list_cmd[1].split(' ', 1)[1]
+            flag_list = False
             for item in list_data:
-                print(f"id:{item['id']} - {item['title']} - {item['msg']} - {item['date']} ")
-                print()
+                if item['date'].split(' ')[0] == date_selected:
+                    print(f"id:{item['id']} - {item['title']} - {item['msg']} - {item['date']} ")
+                    print()
+                    flag_list = True
+            if not flag_list:
+                print('Нет заметок созданных в данную дату')
         else:
-            print('Заметок нет')
+            print('Не верная команда')
 
     elif list_cmd[0].replace(' ', '').lower() == 'remove':
-
         id_remove = list_cmd[1].split(' ', 1)[1]
         file_path = f"note_{id_remove}.json"
         flag_remove = False
@@ -92,6 +133,16 @@ while flag:
                 print(f"Ошибка удаления файла {file_path}: {str(e)}")
         else:
             print('Заметки с таким id не существует')
+
+    elif list_cmd[0].replace(' ', '').lower() == 'read':
+        id_read = list_cmd[1].split(' ', 1)[1]
+        flag_read = False
+        for item in list_data:
+            if int(item['id']) == int(id_read):
+                print(f"id:{item['id']} - {item['title']} - {item['msg']} - {item['date']} ")
+                flag_read = True
+        if not flag_read:
+            print('Нет заметки с таким id')
 
     elif list_cmd[0].replace(' ', '').lower() == 'edit':
 
